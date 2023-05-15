beb1246236ef454862b6ca516d04e5235c74c746
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-05-15 17:22:37 +0300

    tabs

---
+++ src/js/script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items'),
+          tabs = tabsParent.querySelectorAll('.tabheader__item');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.remove('show', 'fade');
+            item.classList.add('hide');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('.tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+
+        tabs[i].classList.add('.tabheader__item_active');
+    }
+    
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+
+
+});
