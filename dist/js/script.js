f2b81611efa21b563fd982f916e9d09a090d1f6c
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-05-16 20:48:15 +0300

    timer modal

--- dist/js/script.js
+++ dist/js/script.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         tabs.forEach(item => {
-            item.classList.remove('.tabheader__item_active');
+            item.classList.remove('tabheader__item_active');
         });
     }
 
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', () => {
         tabsContent[i].classList.remove('hide');
         tabsContent[i].classList.add('show', 'fade');
 
-        tabs[i].classList.add('.tabheader__item_active');
+        tabs[i].classList.add('tabheader__item_active');
     }
     
     hideTabContent();
@@ -40,6 +40,123 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    // Timer
 
+    const deadline = '2023-06-11';
 
+    function getTimeRemaining(endtime) {
+        let days, hours, minutes, seconds;
+
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if(t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }
+        return {
+            total: t,
+            days,
+            hours,
+            minutes,
+            seconds
+        };
+    }
+
+    function zero(num) {
+        if(num > 0 && num <= 10) {
+            return '0' + num;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = zero(t.days);
+            hours.innerHTML = zero(t.hours);
+            minutes.innerHTML = zero(t.minutes);
+            seconds.innerHTML = zero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    // Modal
+
+    function openModal(modalSelector) {
+        const modal = document.querySelector(modalSelector);
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+
+        if(modalTimerId) {
+            clearInterval(modalTimerId);
+        }
+    }
+    
+    function closeModal(modalSelector) {
+        const modal = document.querySelector(modalSelector);
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+    }
+
+    function modal(modalSelector, triggerSelector, modalTimerId) {
+        const modalTrigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector);
+
+        modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => openModal(modalSelector, modalTimerId));
+        });
+
+        modal.addEventListener('click', (e) => {
+            if(e.target === modal || e.target.getAttribute('data-close') == '') {
+                closeModal(modalSelector);
+            }
+        });
+
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape') {
+                closeModal(modalSelector);
+            }
+        });
+
+    }
+
+    const modalTimerId = setTimeout(() => {
+        openModal('.modal', modalTimerId);
+    }, 30000);
+
+    function showModalByScroll() {
+        if(window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal('.modal', modalTimerId);
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+
+    modal('.modal', '[data-modal');
+    
 });
